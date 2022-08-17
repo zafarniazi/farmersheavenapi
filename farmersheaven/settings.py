9a809b91671944c38ef0d601b6de8bd7fd2b8fac
zafar <zafarkhan808045@gmail.com>
2022-08-17 12:53:36 +0500

    db connection change

--- farmersheaven/settings.py
+++ farmersheaven/settings.py
@@ -67,26 +67,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'farmersheaven.wsgi.application'
 
 
-#Database
-#https: // docs.djangoproject.com/en/4.0/ref/settings/  # databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# Database
+# https: // docs.djangoproject.com/en/4.0/ref/settings/  # databases
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'da6fj43uq61e5e',
-#         'USER': 'fbmorhqsnnjyey',
-#         'PASSWORD': '226e223b1313bcbb4fa27b6a850a3930a1338615bb7af0f700677b05bc7ee8a2',
-#         'HOST': 'ec2-44-195-100-240.compute-1.amazonaws.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'da6fj43uq61e5e',
+        'USER': 'fbmorhqsnnjyey',
+        'PASSWORD': '226e223b1313bcbb4fa27b6a850a3930a1338615bb7af0f700677b05bc7ee8a2',
+        'HOST': 'ec2-44-195-100-240.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
